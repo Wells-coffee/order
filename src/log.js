afb10b221c74bc3e2a16e5fe8447d9def12dafcc
wanglingsui <wanglingsui@gmail.com>
2018-03-27 21:02:26 +0800

    c

--- src/log.js
+++ src/log.js
@@ -1,8 +1,10 @@
 /*
  * @Author: lingsui 
  * @Date: 2018-03-27 20:59:23 
- * @Last Modified by:   lingsui 
- * @Last Modified time: 2018-03-27 20:59:23 
+ * @Last Modified by: lingsui
+ * @Last Modified time: 2018-03-27 21:02:15
  */
 
 //  b commit
+
+// c commit
