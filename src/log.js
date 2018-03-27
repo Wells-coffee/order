a2e8f012d48764ff8e16d96f3d06d1d3e4415ee3
wanglingsui <wanglingsui@gmail.com>
2018-03-27 21:11:28 +0800

    f

--- src/log.js
+++ src/log.js
@@ -2,9 +2,11 @@
  * @Author: lingsui 
  * @Date: 2018-03-27 20:59:23 
  * @Last Modified by: lingsui
- * @Last Modified time: 2018-03-27 21:02:15
+ * @Last Modified time: 2018-03-27 21:11:18
  */
 
 //  b commit
 
 // c commit
+
+// f commit
